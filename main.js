9a8554156a4843028b89c1d8bd003993c68ecd91
Vlasya <64101971+Vlasya@users.noreply.github.com>
2020-08-29 16:55:21 +0300

    done 2

--- main.js
+++ main.js
@@ -15,18 +15,19 @@ function genRandomArray(arr=[],min,max){
 		arr[i]=Math.floor(Math.random() * (max - min)) + min;
 	}
 }
-console.log('Массив ',myArray);
+console.log('------------------------------Задание   1  -------------------------------------------');
+console.log('Рандомный массив ',myArray);
 
 // Меняем местами
 function changeItem(arr=[]){
-	myArray.forEach(function(item, index) {
+	arr.forEach(function(item, index) {
 		if(index%2===0){
-			let c=myArray[index];
-			myArray[index]=myArray[index+1];
-			myArray[index+1]=c
+			let c=arr[index];
+			arr[index]=arr[index+1];
+			arr[index+1]=c
 		}
 	 });
-	 return myArray;
+	 return arr;
 }
 
 // Обрезаем массив,если он не парный
@@ -38,4 +39,70 @@ if(myArray.length%2!==0){
 }else{
 	changeItem(myArray);
 }
-console.log('Массив после ',myArray);
+console.log('Массив после смены ',myArray);
+
+
+console.log('------------------------------Задание   2   -------------------------------------------');
+
+
+// В массиве заполненном рандомными числами:
+// Вывести элементы массива, которые больше среднего арифметического
+// Вывести в консоль индексы элементов массива, значение которых лежит в указанном пределе. Предел вводится с клавиатуры как A, B
+// Удалить повторяющиеся элементы из массив, оставить массив уникальных значений
+
+
+myArrayNew= new Array(10);
+genRandomArray(myArrayNew,10,70);
+console.log('рандомный массив задание 2 ',myArrayNew);
+console.log('-    -     -     -     -     -');
+// Вывести элементы массива, которые больше среднего арифметического
+let average = myArrayNew.reduce((sum,item)=>sum+item,0)/myArrayNew.length;
+console.log('Среднее арифметическое массива' ,average);
+
+let moreAvrage= myArrayNew.filter(item=>item>average);
+console.log('Элементы массива, которые больше среднего арифметического: ', moreAvrage);
+
+console.log('-    -     -     -     -     -');
+
+// Вывести в консоль индексы элементов массива, значение которых лежит в указанном пределе. Предел вводится с клавиатуры как A, B
+
+let rangeStart =+prompt('Начало диапазона')
+
+do{
+	var rangeEnd =+prompt('Конец диапазона (Не больше длины массива (в коде 10!!))  и не меньше начала диапазона)');
+} while (rangeEnd>myArrayNew.length1||rangeEnd<rangeStart);
+
+console.log('Начало диапазона: ', rangeStart);
+console.log('Конец диапазона: ', rangeEnd);
+
+
+for(let i=0;i<myArrayNew.length;i++){
+	if(i>=rangeStart && i<=rangeEnd){
+		console.log('Индекс числа: ',i-1, 'Число, на всякий случай ',myArrayNew[i-1]);
+	}
+}
+
+
+console.log('-    -     -     -     -     -');
+// Удалить повторяющиеся элементы из массива, оставить массив уникальных значений
+sort_unicArr(myArrayNew);
+
+function sort_unicArr(arr){
+	 arr=arr.sort(function(a,b){
+		return a-b;
+		console.log(arr,'sort');
+	});
+	var retArr=[];
+	for(var i=0;i<arr.length;i++){
+		if(arr[i-1]!==arr[i]){
+			retArr.push(arr[i]);
+		}
+	}
+	return retArr;
+}
+let myArrayNewUniq=sort_unicArr(myArrayNew);
+
+console.log('Сортируем массив: ', myArrayNew);
+console.log('Только уникальные значения массива: ', myArrayNewUniq);
+console.log('-    -     -     -     -     -');
+
